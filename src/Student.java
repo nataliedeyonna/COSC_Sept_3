bd0816d16d7956762be6a17bf53666619b1050eb
nataliedeyonna <stewartn1004@students.bowiestate.edu>
2026-09-03 10:15:21 -0400

    OC - 3

--- src/Student.java
+++ src/Student.java
@@ -11,17 +11,37 @@ Student(){
     this.Sid = 0;
 }
 
+
+    //Student(String FN, String LN){
+    //    this.FN = FN;
+    //    this.LN = LN;
+   // }
     //How many parameter? 3
         Student(String FN, String LN, int Sid) {
             this.FN = FN;
             this.LN = LN;
             this.Sid = Sid;
         }
+
+    Student(String FN, int Sid, String LN) {
+        this.FN = FN;
+        this.LN = LN;
+        this.Sid = Sid;
+    }
+        //You can create overloaded constructers by varying 1) the parameter number, 2) order
         public static void main(String[] args) {
+
+    Student s4 = new Student( "Helena", "Payton");
+    System.out.println(s4.FN);
+    System.out.println(s4.LN);
+            System.out.println(s4.Sid);
+            System.out.println("---------");
+
             Student s1;
             s1 = new Student();
             System.out.println(s1);
             System.out.println(s1.FN);
+
 
             Student s2;
             s2 = new Student("Tobiloba","Ayodeji", 14141);
